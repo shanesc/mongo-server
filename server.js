e5fd6f5c9858c5dd12683924a15d96ff185f60d7
Shane Mitchell <shanesc@gmail.com>
2021-03-09 17:51:15 +0000

    feat: added environment variables, JSON middleware

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const port = process.env.PORT || 3000;
+
 // SETUP DATABASE
-mongoose.connect('mongodb://localhost/subscribers', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -12,6 +16,7 @@ db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Connected to database'));
 
 // MIDDLEWARE
+app.use(express.json());
 
 // START SERVER
-app.listen(3000, () => console.log('Server started'));
+app.listen(port, () => console.log(`Server started on port ${port}`));

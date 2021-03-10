fed3aa824ddb065024550b89fcc59b31ca5be274
Shane Mitchell <shanesc@gmail.com>
2021-03-10 16:18:03 +0000

    feat: create Subscriber model

---
+++ models/subscriber.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const subscriberSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  subscribedDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Subscriber', subscriberSchema);
